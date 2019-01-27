8a61f70607701d2e9942c77cd2d38e29bebc61c3
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-01-28 01:08:51 +0200

    scripts: set licenses
    All my scripts released under GPL 3.0.

--- scripts/travis-ci-build.sh
+++ scripts/travis-ci-build.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 ##
-## Travis CI: determine updated packages and build them.
+##  Determine updated packages and build them.
+##  Used with Travis CI.
+##
+##  Leonid Plyushch <leonid.plyushch@gmail.com> (C) 2019
+##
+##  This program is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License
+##  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ##
 
 SCRIPT_PATH=$(realpath "$0")
