8c31bd979c08e09344e3ce9e227f8c2fa1b466f6
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-02-28 00:13:01 +0200

    fix start-builder.sh

--- start-builder.sh
+++ start-builder.sh
@@ -54,10 +54,10 @@ fi
 
 		if [ "$(id -u)" -ne 0 ] && [ "$(id -u)" -ne 1000 ]; then
 			echo "Changed builder uid/gid... (this may take a while)"
-			docker exec --tty "$CONTAINER_NAME" chown -R $(id -u) "/home/builder"
-			docker exec --tty "$CONTAINER_NAME" chown -R $(id -u) /data
-			docker exec --tty "$CONTAINER_NAME" usermod -u $(id -u) builder
-			docker exec --tty "$CONTAINER_NAME" groupmod -g $(id -g) builder
+			docker exec --tty "$CONTAINER_NAME" sudo chown -R $(id -u) "/home/builder"
+			docker exec --tty "$CONTAINER_NAME" sudo chown -R $(id -u) /data
+			docker exec --tty "$CONTAINER_NAME" sudo usermod -u $(id -u) builder
+			docker exec --tty "$CONTAINER_NAME" sudo groupmod -g $(id -g) builder
 		fi
 	fi
 
