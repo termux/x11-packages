2cf8a22b864571e3e08650e0f19efdedd75b5845
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-09 20:00:20 +0300

    fix shell path

--- packages/st/config.h
+++ packages/st/config.h
@@ -16,7 +16,7 @@ static int borderpx = 2;
  * 4: value of shell in /etc/passwd
  * 5: value of shell in config.h
  */
-static char *shell = "/bin/sh";
+static char *shell = "@TERMUX_PREFIX@/bin/sh";
 char *utmp = NULL;
 char *stty_args = "stty raw pass8 nl -echo -iexten -cstopb 38400";
 
