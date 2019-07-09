65ada5f14be911e38e784a4afc6ebf466b1d4c04
Tristan Ross <spaceboyross@yandex.com>
2019-07-09 11:04:45 -0700

    fixed dependencies

--- packages/dwm/build.sh
+++ packages/dwm/build.sh
@@ -6,7 +6,7 @@ TERMUX_PKG_VERSION=6.2
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="http://dl.suckless.org/dwm/dwm-$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256=97902e2e007aaeaa3c6e3bed1f81785b817b7413947f1db1d3b62b8da4cd110e
-TERMUX_PKG_DEPENDS="libx11, libxinerama, libxft, freetype2, st, dmenu"
+TERMUX_PKG_DEPENDS="libx11, libxinerama, libxft, freetype, st, dmenu"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_configure() {
