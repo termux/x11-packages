cec538043c4794ee888bea9a0121e7b73ea63b75
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-10 22:11:24 +0300

    fix dependencies

--- packages/polybar/build.sh
+++ packages/polybar/build.sh
@@ -6,9 +6,8 @@ TERMUX_PKG_VERSION=3.3.1
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/polybar/polybar/releases/download/$TERMUX_PKG_VERSION/polybar-$TERMUX_PKG_VERSION.tar"
 TERMUX_PKG_SHA256=b0982a729f99f658d77969fd582d074843a57982b5e7c6b2b6efee5480b2a81c
-TERMUX_PKG_DEPENDS="libandroid-glob, libcairo-x, xcb-util-cursor, xcb-util-image, xcb-util-wm"
+TERMUX_PKG_DEPENDS="fontconfig, freetype, libandroid-glob, libandroid-shmem, libc++, libcairo-x, libcurl, libxcb, xcb-util-cursor, xcb-util-image, xcb-util-xrm, xcb-util-wm"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DENABLE_CURL=OFF -DWITH_XKB=OFF -DWITH_XRANDR=OF -DWITH_XCOMPOSITE=OFF -DWITH_XKB=OFF -DWITH_XCURSOR=OFF"
 
 termux_step_pre_configure() {
 	LDFLAGS+=" -landroid-glob"
