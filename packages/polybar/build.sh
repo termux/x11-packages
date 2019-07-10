0f56bfe206c5b87ca265001c21f138bc1bc67e7f
Tristan Ross <spaceboyross@yandex.com>
2019-07-09 18:21:38 -0700

    added polybar package

---
+++ packages/polybar/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://polybar.github.io
+TERMUX_PKG_DESCRIPTION="A fast and easy-to-use status bar"
+TERMUX_PKG_LICENSE=MIT
+TERMUX_PKG_MAINTAINER="Tristan Ross <spaceboyross@yandex.com>"
+TERMUX_PKG_VERSION=3.3.1
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL="https://github.com/polybar/polybar/releases/download/$TERMUX_PKG_VERSION/polybar-$TERMUX_PKG_VERSION.tar"
+TERMUX_PKG_DEPENDS="cairo, xcb-util-cursor, xcb-util-image, xcb-util-wm"
+TERMUX_PKG_BUILD_IN_SRC=true
