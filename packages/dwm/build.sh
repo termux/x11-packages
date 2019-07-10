cb434008e57b7e026f35dd700b187d3a7460f8b3
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-10 16:13:17 +0300

    some fixes

--- packages/dwm/build.sh
+++ packages/dwm/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://dwm.suckless.org/
 TERMUX_PKG_DESCRIPTION="A dynamic window manager for X"
-TERMUX_PKG_LICENSE=MIT
+TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Tristan Ross <spaceboyross@yandex.com>"
 TERMUX_PKG_VERSION=6.2
 TERMUX_PKG_REVISION=1
@@ -10,5 +10,5 @@ TERMUX_PKG_DEPENDS="libx11, libxinerama, libxft, freetype, st, dmenu"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_configure() {
-  cp "$TERMUX_PKG_BUILDER_DIR/config.h" "config.h"
+	cp "$TERMUX_PKG_BUILDER_DIR/config.h" "config.h"
 }
