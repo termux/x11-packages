38e59a065305723300f899b74c4134b8937d832c
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-07-10 16:08:19 +0300

    fix shell path

--- packages/dwm/config.h
+++ packages/dwm/config.h
@@ -52,7 +52,7 @@ static const Layout layouts[] = {
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
-#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+#define SHCMD(cmd) { .v = (const char*[]){ "@TERMUX_PREFIX@/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
