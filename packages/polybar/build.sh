896055da8986240397c89ac976be316512a9b794
Tristan Ross <spaceboyross@yandex.com>
2019-07-09 18:33:53 -0700

    fixed checksum

--- packages/polybar/build.sh
+++ packages/polybar/build.sh
@@ -5,5 +5,6 @@ TERMUX_PKG_MAINTAINER="Tristan Ross <spaceboyross@yandex.com>"
 TERMUX_PKG_VERSION=3.3.1
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/polybar/polybar/releases/download/$TERMUX_PKG_VERSION/polybar-$TERMUX_PKG_VERSION.tar"
+TERMUX_PKG_SHA256=b0982a729f99f658d77969fd582d074843a57982b5e7c6b2b6efee5480b2a81c
 TERMUX_PKG_DEPENDS="libcairo-x, xcb-util-cursor, xcb-util-image, xcb-util-wm"
 TERMUX_PKG_BUILD_IN_SRC=true
