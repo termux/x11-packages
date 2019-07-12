2b93fd668fdaa8452920bb7148835fbb8b77c8ee
Tristan Ross <spaceboyross@yandex.com>
2019-07-12 10:52:27 -0700

    fixed dependency list

--- packages/zeitgeist/build.sh
+++ packages/zeitgeist/build.sh
@@ -6,6 +6,6 @@ TERMUX_PKG_VERSION=1.0.2
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://launchpad.net/zeitgeist/1.0/$TERMUX_PKG_VERSION/+download/zeitgeist-$TERMUX_PKG_VERSION.tar.xz"
 TERMUX_PKG_SHA256=d023b5904c7076b2701df915dd29b77e256a86d9e80ecfb92f36c9f26371a8ce
-TERMUX_PKG_DEPENDS="gtk3 json-glib telepathy-glib libxaipian"
+TERMUX_PKG_DEPENDS="gtk3, json-glib, telepathy-glib, sqlite, libxaipian"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-fts"
