58843b15a25ad349fd5e7b8d2fa950f201b98770
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-08-21 22:06:14 +0300

    CI: fix for cirrus-ci_dispatcher.sh

--- scripts/cirrus-ci_dispatcher.sh
+++ scripts/cirrus-ci_dispatcher.sh
@@ -196,12 +196,14 @@ if [ "$CIRRUS_BRANCH" = "master" ]; then
 			fi
 		done
 
-		echo "[*] Uploading packages to Bintray:"
-		echo
-		if [ "$LEGACY_ANDROID" = "true" ]; then
-			"${REPO_DIR}/scripts/package_uploader_legacy.sh" -p "${PWD}/debs" $PACKAGE_NAMES
-		else
-			"${REPO_DIR}/scripts/package_uploader.sh" -p "${PWD}/debs" $PACKAGE_NAMES
+		if [ -n "$PACKAGE_NAMES" ]; then
+			echo "[*] Uploading packages to Bintray:"
+			echo
+			if [ "$LEGACY_ANDROID" = "true" ]; then
+				"${REPO_DIR}/scripts/package_uploader_legacy.sh" -p "${PWD}/debs" $PACKAGE_NAMES
+			else
+				"${REPO_DIR}/scripts/package_uploader.sh" -p "${PWD}/debs" $PACKAGE_NAMES
+			fi
 		fi
 	fi
 fi
