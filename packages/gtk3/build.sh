b6cb902f559213526758e1b9549a3aba8fd3b2b2
Leonid Pliushch <leonid.pliushch@gmail.com>
2019-10-14 00:39:26 +0300

    gtk3: depend on ttf-dejavu

--- packages/gtk3/build.sh
+++ packages/gtk3/build.sh
@@ -8,7 +8,7 @@ TERMUX_PKG_SRCURL=https://github.com/GNOME/gtk/archive/${TERMUX_PKG_VERSION}.tar
 TERMUX_PKG_SHA256=456c6b6e316c96fa8cabf72cf216325c9f8c2db8f9e658b0f4d54a67ceaf1d93
 TERMUX_PKG_BUILD_IN_SRC=true
 
-TERMUX_PKG_DEPENDS="adwaita-icon-theme, atk, coreutils, desktop-file-utils, gdk-pixbuf, glib, glib-bin, gtk-update-icon-cache, libcairo-x, libepoxy, libxcomposite, libxcursor, libxdamage, libxfixes, libxi, libxinerama, libxrandr, pango-x, shared-mime-info"
+TERMUX_PKG_DEPENDS="adwaita-icon-theme, atk, coreutils, desktop-file-utils, gdk-pixbuf, glib, glib-bin, gtk-update-icon-cache, libcairo-x, libepoxy, libxcomposite, libxcursor, libxdamage, libxfixes, libxi, libxinerama, libxrandr, pango-x, shared-mime-info, ttf-dejavu"
 TERMUX_PKG_BUILD_DEPENDS="xorgproto"
 TERMUX_PKG_CONFLICTS="libgtk3"
 TERMUX_PKG_REPLACES="libgtk3"
