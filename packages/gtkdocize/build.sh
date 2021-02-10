27d197aca54aa05468c12a9ce4ceec34880fe3e3
Ludea <ludealol.olol@gmail.com>
2021-02-10 09:29:07 +0100

    gtkdocize

---
+++ packages/gtkdocize/build.sh
@@ -0,0 +1,10 @@
+TERMUX_PKG_HOMEPAGE=https://gitlab.gnome.org/GNOME/gtk-doc-stub
+TERMUX_PKG_DESCRIPTION="gtkdocize prepares a source package to use gtk-doc"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_VERSION=1.1
+TERMUX_PKG_SRCURL=https://gitlab.gnome.org/GNOME/gtk-doc-stub/-/archive/master/gtk-doc-stub-master.tar.gz
+TERMUX_PKG_SHA256=959d22ae07f22803bc66ff40d373a854532a6e4732680bf8a96a3fbcb9f80a2c
+
+
+
+
