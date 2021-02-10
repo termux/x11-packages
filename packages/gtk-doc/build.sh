21a793e285b018aecd389ecd0af68fd5ae0c443e
Ludea <ludealol.olol@gmail.com>
2021-02-10 13:24:53 +0100

    Gtk-doc

---
+++ packages/gtk-doc/build.sh
@@ -0,0 +1,6 @@
+TERMUX_PKG_HOMEPAGE=https://gitlab.gnome.org/GNOME/gtk-doc
+TERMUX_PKG_DESCRIPTION=GTK+ DocBook Documentation Generator
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_VERSION=1.33.2
+TERMUX_PKG_SRCURL=https://gitlab.gnome.org/GNOME/gtk-doc/-/archive/${TERMUX_PKG_VERSION}/gtk-doc-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=959d22ae07f22803bc66ff40d373a854532a6e4732680bf8a96a3fbcb9f80a2c
