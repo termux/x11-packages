c42345810a46ef202ecfbf0620594462c12f1500
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-02-10 02:10:51 +0200

    linter: accept free-form data in license field
    Though SPDX identifiers are prefered.

--- scripts/lint-packages.sh
+++ scripts/lint-packages.sh
@@ -22,51 +22,6 @@ set -e -u
 REPO_DIR=$(realpath "$(dirname "$0")/../")
 PACKAGES_DIR="$REPO_DIR/packages"
 
-check_package_license() {
-	local pkg_licenses=$1
-	local license
-	local license_ok=true
-
-	IFS=","
-	for license in $pkg_licenses; do
-		license=$(echo "$license" | sed -r 's/^\s*(\S+(\s+\S+)*)\s*$/\1/')
-
-		case "$license" in
-			AFL-2.1|AFL-3.0|AGPL-V3|APL-1.0|APSL-2.0|Apache-1.0|Apache-1.1);;
-			Apache-2.0|Artistic-License-2.0|Attribution|BSD|"BSD 2-Clause");;
-			"BSD 3-Clause"|"BSD New"|"BSD Simplified"|BSL-1.0|Bouncy-Castle);;
-			CA-TOSL-1.1|CC0-1.0|CDDL-1.0|CDDL-1.1|CPAL-1.0|CPL-1.0|CPOL);;
-			CPOL-1.02|CUAOFFICE-1.0|CeCILL-1|CeCILL-2|CeCILL-2.1|CeCILL-B);;
-			CeCILL-C|Codehaus|Copyfree|Day|Day-Addendum|ECL2|EPL-1.0|EPL-2.0);;
-			EUDATAGRID|EUPL-1.1|EUPL-1.2|Eiffel-2.0|Entessa-1.0);;
-			Facebook-Platform|Fair|Frameworx-1.0|GPL-2.0|GPL-3.0|GPL-3.0-only);;
-			GPL-3.0-or-later|Go|HSQLDB|Historical|IBMPL-1.0|IJG|IPAFont-1.0);;
-			ISC|IU-Extreme-1.1.1|ImageMagick|JA-SIG|JSON|JTidy|LGPL-2.0);;
-			LGPL-2.1|LGPL-3.0|LPPL-1.0|Libpng|Lucent-1.02|MIT|MPL-2.0|MS-PL);;
-			MS-RL|MirOS|Motosoto-0.9.1|Mozilla-1.1|Multics|NASA-1.3|NAUMEN);;
-			NCSA|NOSL-3.0|NTP|NUnit-2.6.3|NUnit-Test-Adapter-2.6.3|Nethack);;
-			Nokia-1.0a|OCLC-2.0|OSL-3.0|OpenLDAP|OpenSSL|Openfont-1.1);;
-			Opengroup|PHP-3.0|PostgreSQL|"Public Domain"|"Public Domain - SUN");;
-			PythonPL|PythonSoftFoundation|QTPL-1.0|RPL-1.5|Real-1.0|RicohPL);;
-			SUNPublic-1.0|Scala|SimPL-2.0|Sleepycat|Sybase-1.0|TMate|UPL-1.0);;
-			Unicode-DFS-2015|Unlicense|UoI-NCSA|"VIM License"|VovidaPL-1.0|W3C);;
-			WTFPL|Xnet|ZLIB|ZPL-2.0|wxWindows);;
-
-			*)
-				license_ok=false
-				break
-				;;
-		esac
-	done
-	IFS=$old_ifs
-
-	if $license_ok; then
-		return 0
-	else
-		return 1
-	fi
-}
-
 lint_package() {
 	local package_script
 	local package_name
@@ -142,18 +97,7 @@ lint_package() {
 
 		echo -n "TERMUX_PKG_LICENSE: "
 		if [ -n "$TERMUX_PKG_LICENSE" ]; then
-			if [ "$TERMUX_PKG_LICENSE" = "custom" ]; then
-				echo "CUSTOM"
-			elif [ "$TERMUX_PKG_LICENSE" = "non-free" ]; then
-				echo "NON-FREE"
-			else
-				if check_package_license "$TERMUX_PKG_LICENSE"; then
-					echo "PASS"
-				else
-					echo "INVALID"
-					pkg_lint_error=true
-				fi
-			fi
+			echo "PASS"
 		else
 			echo "NOT SET"
 			pkg_lint_error=true
