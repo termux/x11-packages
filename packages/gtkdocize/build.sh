972578376055201009b1ced203bb2c72aefda599
Ludea <ludealol.olol@gmail.com>
2021-02-10 09:47:25 +0100

    Right checksum

--- packages/gtkdocize/build.sh
+++ packages/gtkdocize/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="gtkdocize prepares a source package to use gtk-doc"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=1.1
 TERMUX_PKG_SRCURL=https://gitlab.gnome.org/GNOME/gtk-doc-stub/-/archive/master/gtk-doc-stub-master.tar.gz
-TERMUX_PKG_SHA256=959d22ae07f22803bc66ff40d373a854532a6e4732680bf8a96a3fbcb9f80a2c
+TERMUX_PKG_SHA256=bf0181629a9263f6f6f8f0a0ff643f1dee5cb7a63cf6a4d339ea84868d841d97
 
 
 
