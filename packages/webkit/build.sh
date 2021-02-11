ff5c6d50aa9afb6b4aeaf86f1b8e2eab46e0d7ee
Ludea <ludealol.olol@gmail.com>
2021-02-11 18:13:16 +0100

    webp deps

--- packages/webkit/build.sh
+++ packages/webkit/build.sh
@@ -6,4 +6,4 @@ TERMUX_PKG_VERSION=2.31.1
 TERMUX_PKG_SRCURL=https://webkitgtk.org/releases/webkitgtk-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DPORT=GTK -DCMAKE_BUILD_TYPE=RelWithDebInfo"
 TERMUX_PKG_SHA256=6b1bb3e0efcfcb6e4a8e18b6a5f1cac27cda203d46a7dfbb0f150784a47e908f
-TERMUX_PKG_BUILD_DEPENDS="libcairo, libgcrypt, gtk3, libsoup" 
+TERMUX_PKG_BUILD_DEPENDS="libcairo, libgcrypt, gtk3, libsoup, webp" 
