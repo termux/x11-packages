10b1fbf8cf417e2cddecdfc6c01df5a10c3e11f9
Ludea <ludealol.olol@gmail.com>
2021-02-19 10:51:16 +0100

    Typo

--- packages/webkit/build.sh
+++ packages/webkit/build.sh
@@ -6,4 +6,4 @@ TERMUX_PKG_VERSION=2.31.1
 TERMUX_PKG_SRCURL=https://webkitgtk.org/releases/webkitgtk-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DPORT=GTK -DCMAKE_BUILD_TYPE=RelWithDebInfo -DENABLE_GAMEPAD=OFF -DUSE_SYSTEMD=OFF -DUSE_LIBSECRET=OFF -DENABLE_INTROSPECTION=OFF"
 TERMUX_PKG_SHA256=6b1bb3e0efcfcb6e4a8e18b6a5f1cac27cda203d46a7dfbb0f150784a47e908f
-TERMUX_PKG_DEPENDS="libtasn1, libxt libcairo, libgcrypt, gtk3, libsoup, libwebp, libxslt, woff2, enchant, libhyphen, gst-plugins-base, gstreamer" 
+TERMUX_PKG_DEPENDS="libtasn1, libxt, libcairo, libgcrypt, gtk3, libsoup, libwebp, libxslt, woff2, enchant, libhyphen, gst-plugins-base, gstreamer" 
