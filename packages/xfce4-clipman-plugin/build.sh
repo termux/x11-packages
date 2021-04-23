7751e685cd2be0f5e0e9d58a3f994ab5c279dfd6
suhan-paradkar <76830230+suhan-paradkar@users.noreply.github.com>
2021-04-23 08:49:06 +0530

    Delete build.sh

--- packages/xfce4-clipman-plugin/build.sh
+++
@@ -1,7 +0,0 @@
-TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/panel-plugins/xfce4-clipman-plugin/start
-TERMUX_PKG_DESCRIPTION="Clipman is a clipboard manager for Xfce"
-TERMUX_PKG_LICENSE="GPL_2.0"
-TERMUX_PKG_VERSION=1.6.1
-TERMUX_PKG_SRCURL=https://archive.xfce.org/src/panel-plugins/xfce4-clipman-plugin/1.6/xfce4-clipman-plugin-$TERMUX_PKG_VERSION.tar.bz2
-TERMUX_PKG_SHA256=b3e8317796289196fa78c4526626fe061fd35d11e848cf300ca1ace47febceb4
-TERMUX_PKG_DEPENDS="libxfce4util, libxtst, xournal, libxfce4ui, xfce4-panel"
