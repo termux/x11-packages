4df26337d75f76a64f8a7c2aab0b360d6b194b89
suhan-paradkar <12suhangp34@gmail.com>
2021-05-07 09:57:32 +0000

    add package: wayland

---
+++ packages/wayland/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://wayland.freedesktop.org/
+TERMUX_PKG_DESCRIPTION="Wayland is intended as a simpler replacement for X, easier to develop and maintain."
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_VERSION=1.19.0
+TERMUX_PKG_SRCURL=https://wayland.freedesktop.org/releases/wayland-$TERMUX_PKG_VERSION.tar.xz
+TERMUX_PKG_SHA256=baccd902300d354581cd5ad3cc49daa4921d55fb416a5883e218750fef166d15
+TERMUX_PKG_DEPENDS="libexpat, doxygen, graphviz"
