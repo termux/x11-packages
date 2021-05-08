f82c62960279f5b809ea1e0e95ab310fc57f4eb1
suhan-paradkar <12suhangp34@gmail.com>
2021-05-08 09:46:54 +0000

    add package: buoh

---
+++ packages/buoh/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=http://buoh.steve-o.org/
+TERMUX_PKG_DESCRIPTION="Buoh aims to help the comic lovers by providing an easy way of browsing and reading their favourites online comic strips."
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_VERSION=0.8.3
+TERMUX_PKG_SRCURL=https://gitlab.gnome.org/GNOME/buoh.git
+TERMUX_PKG_GIT_BRANCH=master
+TERMUX_PKG_DEPENDS="glib, gtk3, libxml2, libsoup"
+
