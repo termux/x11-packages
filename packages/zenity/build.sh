7ef80be00e1643ff860e1491005291a380b4a990
NAME <mail@gmail.com>
2021-05-10 19:32:47 +0530

    correct build.sh

--- packages/zenity/build.sh
+++ packages/zenity/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://gitlab.gnome.org/GNOME/zenity
 TERMUX_PKG_DESCRIPTION="a rewrite of gdialog, the GNOME port of dialog"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=3_32_0
-TERMUX_PKG_SRCURL=https://github.com/GNOME/zenity/archive/refs/tags/ZENITY_$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_VERSION=3.32.0
+TERMUX_PKG_SRCURL=https://github.com/GNOME/zenity/archive/refs/tags/ZENITY_${TERMUX_PKG_VERSION/./_}.tar.gz
 TERMUX_PKG_SHA256=9329e6a2d114316a7b75202d9d942c2e5ee2351571375cc0eda8e868598c07c5
 termux_step_pre_configure() {
 	autoreconf -fi
