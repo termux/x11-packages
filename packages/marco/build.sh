a5b456e6b0996daf709942f4b819a7bb23f6101c
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-05-11 14:54:04 +0300

    Update build.sh

--- packages/marco/build.sh
+++ packages/marco/build.sh
@@ -5,3 +5,7 @@ TERMUX_PKG_VERSION=1.24.2
 TERMUX_PKG_SRCURL=https://github.com/mate-desktop/marco/releases/download/v$TERMUX_PKG_VERSION/marco-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=498350cb12fc4d9c7c4a0355102c17b50043fbab258d11ad41b7d698b4f042a7
 TERMUX_PKG_DEPENDS="libcanberra, glib, gtk3, zenity"
+
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+ZENITY=${TERMUX_PREFIX}/bin/zenity
+"
