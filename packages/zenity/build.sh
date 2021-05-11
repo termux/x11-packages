a239499dc823c74fad021096175033e49be6d13f
suhan-paradkar <12suhangp34@gmail.com>
2021-05-11 11:07:00 +0000

    update source

--- packages/zenity/build.sh
+++ packages/zenity/build.sh
@@ -1,9 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://gitlab.gnome.org/GNOME/zenity
 TERMUX_PKG_DESCRIPTION="a rewrite of gdialog, the GNOME port of dialog"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=3.32.0
-TERMUX_PKG_SRCURL=https://github.com/GNOME/zenity/archive/refs/tags/ZENITY_${TERMUX_PKG_VERSION/./_}.tar.gz
-TERMUX_PKG_SHA256=9329e6a2d114316a7b75202d9d942c2e5ee2351571375cc0eda8e868598c07c5
+_MAJOR_VERSION=3.32
+TERMUX_PKG_VERSION=$_MAJOR_VERSION.0
+TERMUX_PKG_SRCURL=https://download.gnome.org/sources/zenity/$_MAJOR_VERSION/zenity-$TERMUX_PKG_VERSION.tar.xz
+TERMUX_PKG_SHA256=e786e733569c97372c3ef1776e71be7e7599ebe87e11e8ad67dcc2e63a82cd95
 TERMUX_PKG_DEPENDS="glib, gtk3"
 termux_step_pre_configure() {
 	autoreconf -fi
