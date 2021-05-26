73625b4b2008d5db525829fb585a7dc4e2e5022d
Suhan G Paradkar <76830230+suhan-paradkar@users.noreply.github.com>
2021-05-26 15:10:08 +0530

    Add gtk3 dependency for libmateweather

--- packages/libmateweather/build.sh
+++ packages/libmateweather/build.sh
@@ -4,4 +4,4 @@ TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=1.24.1
 TERMUX_PKG_SRCURL=https://github.com/mate-desktop/libmateweather/releases/download/v$TERMUX_PKG_VERSION/libmateweather-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=9b4cfdefcd368137b9300e19fd6ed31b26a56336b78ef3fa772156755361a709
-TERMUX_PKG_DEPENDS="libxml2, libsoup"
+TERMUX_PKG_DEPENDS="libxml2, libsoup, gtk3"
