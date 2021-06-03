18680a7a84120912ac544afc66509db67b4b7834
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-06-03 20:42:43 +0300

    at-spi2-gtk: fix package description

--- packages/at-spi2-atk/build.sh
+++ packages/at-spi2-atk/build.sh
@@ -1,5 +1,5 @@
 TERMUX_PKG_HOMEPAGE=https://wiki.linuxfoundation.org/accessibility/atk/at-spi/at-spi_on_d-bus
-TERMUX_PKG_DESCRIPTION="GNOME Shell provides core user interface functions for the GNOME 3 desktop"
+TERMUX_PKG_DESCRIPTION="A GTK+ module that bridges ATK to D-Bus at-spi"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=2.38.0
 TERMUX_PKG_SRCURL=https://gitlab.gnome.org/GNOME/at-spi2-atk.git
