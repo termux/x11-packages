a5d6555ee61332bb0200549a1f7e65c382832dac
Leap of Azzam <76893443+leapofazzam123@users.noreply.github.com>
2021-06-08 11:19:34 +0700

    Update build.sh

--- packages/sxhkd/build.sh
+++ packages/sxhkd/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE="https://github.com/baskerville/sxhkd"
 TERMUX_PKG_DESCRIPTION="Simple X hotkey daemon"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
-TERMUX_PKG_MAINTAINER="Leap of Azzam (@leapofazzam-backup)"
+TERMUX_PKG_MAINTAINER="Leap of Azzam (@leapofazzam123)"
 TERMUX_PKG_VERSION=0.6.2
 TERMUX_PKG_SRCURL="https://github.com/baskerville/sxhkd/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256="1edc8b1a8b3631c10ba9cb9df1181830dacbbdf77adb558e31d5dd2029637386"
