e34a6ca16aafcbd16094611d17dcd9109eeb3b44
Yisus7u7 <jesuspixel5@gmail.com>
2021-06-18 11:43:42 -0500

    fix

--- packages/papirus-icon-theme/build.sh
+++ packages/papirus-icon-theme/build.sh
@@ -1,16 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/PapirusDevelopmentTeam/papirus-icon-theme
-TERMUX_PKG_DESCRIPTION="Papirus open source icon theme for Linux
- Papirus is a SVG-based icon theme, drawing inspiration from Material Design
- and flat design.
- .
- This package contains the following icon themes:
-  - Papirus
-  - Papirus-Dark
-  - Papirus-Light
-  - ePapirus"
+TERMUX_PKG_DESCRIPTION="Papirus is a free and open source SVG icon theme for Linux, based on Paper Icon Set with a lot of new icons and a few extras"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_VERSION=20210601
 TERMUX_PKG_SRCURL=https://github.com/PapirusDevelopmentTeam/papirus-icon-theme/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_SHA256=e39f4212e7433010b6246f5a2989a9e263c7ab8029a5d1759f2744f4a54b6b14
 TERMUX_PKG_DEPENDS="hicolor-icon-theme"
