5a4c496c80efd3a3e44c5d0f43f904127a8ef0ad
Yisus7u7 <jesuspixel5@gmail.com>
2021-06-18 01:49:34 -0500

    add papirus-icon-theme

---
+++ packages/papirus-icon-theme/build.sh
@@ -0,0 +1,16 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/PapirusDevelopmentTeam/papirus-icon-theme
+TERMUX_PKG_DESCRIPTION="Papirus open source icon theme for Linux
+ Papirus is a SVG-based icon theme, drawing inspiration from Material Design
+ and flat design.
+ .
+ This package contains the following icon themes:
+  - Papirus
+  - Papirus-Dark
+  - Papirus-Light
+  - ePapirus"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_VERSION=20210601
+TERMUX_PKG_SRCURL=https://github.com/PapirusDevelopmentTeam/papirus-icon-theme/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_SHA256=e39f4212e7433010b6246f5a2989a9e263c7ab8029a5d1759f2744f4a54b6b14
+TERMUX_PKG_DEPENDS="hicolor-icon-theme"
