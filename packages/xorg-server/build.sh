46ae1acfda8b90f4b2fb4f49a289358689c5f094
Suhan G Paradkar <76830230+suhan-paradkar@users.noreply.github.com>
2021-06-19 14:38:11 +0530

    xorg-server:Blacklist i686

--- packages/xorg-server/build.sh
+++ packages/xorg-server/build.sh
@@ -5,6 +5,11 @@ TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/xserver/xorg-server-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=914c796e3ffabe1af48071d40ccc85e92117c97a9082ed1df29e4d64e3c34c49
 
+#i686 gives the following error...
+#relocation R_386_GOTOFF against preemptible symbol fbdevHWLoadPalette cannot be used when making a shared object
+
+TERMUX_PKG_BLACKLISTED_ARCHES="i686"
+
 TERMUX_PKG_DEPENDS="libandroid-shmem, libdrm, libpciaccess, libpixman, libx11, libxau, libxfont2, libxinerama, libxkbfile, libxshmfence, mesa, openssl, xkeyboard-config, xorg-xkbcomp"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
