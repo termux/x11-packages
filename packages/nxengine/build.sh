97f984498b1f2c2ec93c6229fa39167227304f0f
yonle <fledyonle@gmail.com>
2021-07-05 16:36:00 +0700

    Fix For 3rd time

--- packages/nxengine/build.sh
+++ packages/nxengine/build.sh
@@ -7,6 +7,7 @@ TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://github.com/EXL/NXEngine/archive/refs/tags/v1.0.0.4-Rev4.tar.gz
 TERMUX_PKG_SHA256=d467c112e81d4c56337ebf6968bd8bd781bce9140f674e72009a5274d2c15784
 TERMUX_PKG_BUILD_DEPENDS="sdl, sdl-ttf"
+TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
         make -f Makefile.linux
