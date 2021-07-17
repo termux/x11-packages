80f963f57e832f86e694710ec9fc8b521ff33509
Yisus7u7 <jesuspixel5@gmail.com>
2021-07-17 13:49:21 -0500

    fix missing deps

--- packages/roxterm/build.sh
+++ packages/roxterm/build.sh
@@ -5,5 +5,5 @@ TERMUX_PKG_MAINTAINER="Yisus7u7"
 TERMUX_PKG_VERSION=3.11.1
 TERMUX_PKG_SRCURL=https://github.com/realh/roxterm/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=a362d4b6ca89091d277c710c6a4dcec67a9429d519086b911941b66af1e8e3e3
-TERMUX_PKG_DEPENDS="gtk3, libvte, xsltproc, pcre2, glib"
+TERMUX_PKG_DEPENDS="gtk3, libvte, xsltproc, pcre2, dbus-glib"
 TERMUX_PKG_BUILD_IN_SRC=true
