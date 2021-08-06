87082f3d23b48947d1c6f11d71c59ae18c7b025a
Yisus7u7 <jesuspixel5@gmail.com>
2021-08-05 19:49:22 -0500

    fix missing dep

--- packages/glade/build.sh
+++ packages/glade/build.sh
@@ -5,4 +5,4 @@ TERMUX_PKG_MAINTAINER="Yisus7u7"
 TERMUX_PKG_VERSION=3.38.2
 TERMUX_PKG_SRCURL=https://github.com/GNOME/glade/archive/refs/tags/GLADE_3_38_2.tar.gz
 TERMUX_PKG_SHA256=8abd1e7e6f943156602c6591e763ff40cffc68ea144c523d9702276f54b91b1b
-TERMUX_PKG_DEPENDS="atk, fontconfig, freetype, gdk-pixbuf, glib, gtk3, libcairo, libxml2, pango, xsltproc, gobject-introspection"
+TERMUX_PKG_DEPENDS="atk, fontconfig, freetype, gdk-pixbuf, glib, gtk3, libcairo, libxml2, pango, xsltproc, gobject-introspection, libglade"
