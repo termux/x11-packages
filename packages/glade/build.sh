571754beb5f1ff6c1dec59daf308db817525cd9b
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-08-06 18:34:24 +0300

    Update build.sh

--- packages/glade/build.sh
+++ packages/glade/build.sh
@@ -6,4 +6,4 @@ TERMUX_PKG_VERSION=3.38.2
 TERMUX_PKG_SRCURL=https://github.com/GNOME/glade/archive/refs/tags/GLADE_3_38_2.tar.gz
 TERMUX_PKG_SHA256=8abd1e7e6f943156602c6591e763ff40cffc68ea144c523d9702276f54b91b1b
 TERMUX_PKG_DEPENDS="atk, fontconfig, freetype, gdk-pixbuf, glib, gtk3, libcairo, libxml2, pango, xsltproc, libglade"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS+="-Dintrospection=false" 
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dintrospection=false" 
