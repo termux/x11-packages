8fe29a28d2f04e7f62ea714b0f88b55ca8506d31
LayaAchmad <86137443+layaachmad@users.noreply.github.com>
2021-08-17 05:20:12 +0700

    Update build.sh

--- packages/tint2/build.sh
+++ packages/tint2/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_MAINTAINER="Laya Achmad"
 TERMUX_PKG_VERSION=16.2
 TERMUX_PKG_SRCURL=https://github.com/o9000/tint2/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=a95c8a22d4cc117335b3157c7de48da91c8721fc97f86d0353c545eed2598ce8
-TERMUX_PKG_DEPENDS="xorg-xrandr, libxinerama, libx11, xorgproto, libandroid-glob, libandroid-shmem, pango, libc++, libcairo, libcurl, libxcb, xcb-util-cursor, xcb-util-image, xcb-util-xrm, xcb-util-wm, pulseaudio, libxcomposite, libxdamage, gtk2, gtk3 imlib2, libandroid-wordexp, librsvg, startup-notification, libcairo"
+TERMUX_PKG_DEPENDS="xorg-xrandr, libxinerama, libx11, xorgproto, libandroid-glob, libandroid-shmem, pango, libc++, libcairo, libcurl, libxcb, xcb-util-cursor, xcb-util-image, xcb-util-xrm, xcb-util-wm, pulseaudio, libxcomposite, libxdamage, gtk2, imlib2, libandroid-wordexp, librsvg, startup-notification, libcairo"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
