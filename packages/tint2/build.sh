d949cca1fe1156e02f91ff13f6f1399e67585d39
LayaAchmad <86137443+layaachmad@users.noreply.github.com>
2021-08-16 21:22:19 +0700

    fix build.sh

--- packages/tint2/build.sh
+++ packages/tint2/build.sh
@@ -6,7 +6,6 @@ TERMUX_PKG_VERSION=16.2
 TERMUX_PKG_SRCURL=https://github.com/o9000/tint2/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=a95c8a22d4cc117335b3157c7de48da91c8721fc97f86d0353c545eed2598ce8
 TERMUX_PKG_DEPENDS="xorg-xrandr, libxinerama, libx11, xorgproto, libandroid-glob, libandroid-shmem, libc++, libcairo, libcurl, libxcb, xcb-util-cursor, xcb-util-image, xcb-util-xrm, xcb-util-wm, pulseaudio, libandroid-wordexp"
-TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
 	LDFLAGS+=" -landroid-glob -landroid-shmem -landroid-wordexp"
