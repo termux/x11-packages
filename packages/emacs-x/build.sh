ae0c897350d1d99755bfbe5d1c2da1a2ee3e86bb
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-08-25 14:46:27 +0300

    enable package: emacs-x
    Fixes https://github.com/termux/x11-packages/issues/251

--- packages/emacs-x/build.sh
+++ packages/emacs-x/build.sh
@@ -1,29 +1,14 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/emacs/
-TERMUX_PKG_DESCRIPTION="Extensible, customizable text editor-and more (with X11 support)"
+TERMUX_PKG_DESCRIPTION="Extensible, customizable text editor-and more"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=26.3
-TERMUX_PKG_REVISION=17
-TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/emacs/emacs-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=4d90e6751ad8967822c6e092db07466b9d383ef1653feb2f95c93e7de66d3485
+TERMUX_PKG_VERSION=27.2
+TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/emacs/emacs-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=b4a7cc4e78e63f378624e0919215b910af5bb2a0afc819fad298272e9f40c1b9
 TERMUX_PKG_DEPENDS="fontconfig, freetype, gdk-pixbuf, giflib, glib, libgnutls, libice, libjpeg-turbo, libpng, librsvg, libsm, libtiff, libx11, libxaw, libxcb, libxext, libxfixes, libxft, libxinerama, libxml2, libxmu, libxpm, libxrandr, libxrender, libxt, littlecms, ncurses, zlib"
-TERMUX_PKG_CONFLICTS="emacs"
-TERMUX_PKG_REPLACES="emacs"
-TERMUX_PKG_PROVIDES="emacs"
-TERMUX_PKG_CONFFILES="var/service/emacsd/run var/service/emacsd/log/run"
-TERMUX_PKG_HOSTBUILD=true
-
-# Remove some irrelevant files:
-TERMUX_PKG_RM_AFTER_INSTALL="
-bin/ctags
-bin/grep-changelog
-share/emacs/${TERMUX_PKG_VERSION}/etc/images
-share/emacs/${TERMUX_PKG_VERSION}/etc/refcards
-share/info/dir
-share/man/man1/ctags.1
-share/man/man1/ctags.1.gz
-share/man/man1/grep-changelog.1.gz"
-
+TERMUX_PKG_BREAKS="emacs-dev"
+TERMUX_PKG_REPLACES="emacs-dev"
+TERMUX_PKG_SERVICE_SCRIPT=("emacsd" 'exec emacs --fg-daemon 2>&1')
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-autodepend
 --without-cairo
@@ -36,23 +21,57 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --without-gsettings
 --with-x
 --with-x-toolkit=lucid
+--without-selinux
+--with-modules
+--with-pdumper=yes
+--with-dumping=none
 "
+
+if $TERMUX_DEBUG_BUILD; then
+	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+="
+	--enable-checking=yes,glyphs
+	--enable-check-lisp-object-type
+	"
+	CFLAGS+=" -gdwarf-4"
+fi
+
 # Ensure use of system malloc:
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" emacs_cv_sanitize_address=yes"
-
 # Prevent configure from adding -nopie:
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" emacs_cv_prog_cc_no_pie=no"
-
 # Prevent linking against libelf:
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" ac_cv_lib_elf_elf_begin=no"
-
 # implemented using dup3(), which fails if oldfd == newfd
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" gl_cv_func_dup2_works=no"
-
 # disable setrlimit function to make termux-am work from within emacs
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" ac_cv_func_setrlimit=no"
+if [ "$TERMUX_ARCH" == "arm" ] || [ "$TERMUX_ARCH" == "i686" ]; then
+	# setjmp does not work properly on 32bit android:
+	# https://github.com/termux/termux-packages/issues/2599
+	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" emacs_cv_func__setjmp=no"
+	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" emacs_cv_func_sigsetjmp=no"
+fi
+TERMUX_PKG_HOSTBUILD=true
 
-termux_step_post_extract_package() {
+# Remove some irrelevant files:
+TERMUX_PKG_RM_AFTER_INSTALL="
+bin/grep-changelog
+share/applications/emacs.desktop
+share/emacs/${TERMUX_PKG_VERSION}/etc/emacs.desktop
+share/emacs/${TERMUX_PKG_VERSION}/etc/emacs.icon
+share/emacs/${TERMUX_PKG_VERSION}/etc/images
+share/emacs/${TERMUX_PKG_VERSION}/etc/refcards
+share/emacs/${TERMUX_PKG_VERSION}/etc/tutorials/TUTORIAL.*
+share/icons
+share/man/man1/grep-changelog.1.gz
+"
+
+# Remove ctags from the emacs package to prevent conflicting with
+# the Universal Ctags from the 'ctags' package (the bin/etags
+# program still remain in the emacs package):
+TERMUX_PKG_RM_AFTER_INSTALL+=" bin/ctags share/man/man1/ctags.1 share/man/man1/ctags.1.gz"
+
+termux_step_post_get_source() {
 	# Certain packages are not safe to build on device because their
 	# build.sh script deletes specific files in $TERMUX_PREFIX.
 	if $TERMUX_ON_DEVICE_BUILD; then
@@ -66,11 +85,6 @@ termux_step_post_extract_package() {
 	# Termux only use info pages for emacs. Remove the info directory
 	# to get a clean Info directory file dir.
 	rm -Rf $TERMUX_PREFIX/share/info
-
-	# We cannot run a dumped emacs on Android 5.0+ due to the pie requirement.
-	# Also, the native emacs we build (bootstrap-emacs) cannot used dumps when
-	# building inside docker: https://github.com/docker/docker/issues/22801
-	export CANNOT_DUMP=yes
 }
 
 termux_step_host_build() {
@@ -78,31 +92,23 @@ termux_step_host_build() {
 	local NATIVE_PREFIX=$TERMUX_PKG_TMPDIR/emacs-native
 	mkdir -p $NATIVE_PREFIX/share/emacs/$TERMUX_PKG_VERSION
 	ln -s $TERMUX_PKG_SRCDIR/lisp $NATIVE_PREFIX/share/emacs/$TERMUX_PKG_VERSION/lisp
-
-	$TERMUX_PKG_SRCDIR/configure --prefix=$NATIVE_PREFIX --without-all --with-x-toolkit=no
+	( cd $TERMUX_PKG_SRCDIR; ./autogen.sh )
+	$TERMUX_PKG_SRCDIR/configure --prefix=$NATIVE_PREFIX --without-all --without-x
 	make -j $TERMUX_MAKE_PROCESSES
 }
 
 termux_step_post_configure() {
 	cp $TERMUX_PKG_HOSTBUILD_DIR/src/bootstrap-emacs $TERMUX_PKG_BUILDDIR/src/bootstrap-emacs
 	cp $TERMUX_PKG_HOSTBUILD_DIR/lib-src/make-docfile $TERMUX_PKG_BUILDDIR/lib-src/make-docfile
+	cp $TERMUX_PKG_HOSTBUILD_DIR/lib-src/make-fingerprint $TERMUX_PKG_BUILDDIR/lib-src/make-fingerprint
 	# Update timestamps so that the binaries does not get rebuilt:
-	touch -d "next hour" $TERMUX_PKG_BUILDDIR/src/bootstrap-emacs $TERMUX_PKG_BUILDDIR/lib-src/make-docfile
+	touch -d "next hour" $TERMUX_PKG_BUILDDIR/src/bootstrap-emacs \
+		$TERMUX_PKG_BUILDDIR/lib-src/make-docfile \
+		$TERMUX_PKG_BUILDDIR/lib-src/make-fingerprint
 }
 
 termux_step_post_make_install() {
 	cp $TERMUX_PKG_BUILDER_DIR/site-init.el $TERMUX_PREFIX/share/emacs/${TERMUX_PKG_VERSION}/lisp/emacs-lisp/
-
-	# Setup emacs --daemon service script
-	mkdir -p $TERMUX_PREFIX/var/service
-	cd $TERMUX_PREFIX/var/service
-	mkdir -p emacsd/log
-	echo "#!$TERMUX_PREFIX/bin/sh" > emacsd/run
-	echo 'exec emacs --fg-daemon 2>&1' >> emacsd/run
-	chmod +x emacsd/run
-	touch emacsd/down
-
-	ln -sf $TERMUX_PREFIX/share/termux-services/svlogger emacsd/log/run
 }
 
 termux_step_create_debscripts() {
@@ -114,6 +120,10 @@ termux_step_create_debscripts() {
 				$TERMUX_PREFIX/bin/editor editor $TERMUX_PREFIX/bin/emacs 40
 		fi
 	fi
+
+	cd $TERMUX_PREFIX/share/emacs/$TERMUX_PKG_VERSION/lisp
+	LC_ALL=C $TERMUX_PREFIX/bin/emacs -batch -l loadup --temacs=pdump
+	mv $TERMUX_PREFIX/bin/emacs*.pdmp $TERMUX_PREFIX/libexec/emacs/$TERMUX_PKG_VERSION/${TERMUX_ARCH}-linux-android*/
 	EOF
 
 	cat <<- EOF > ./prerm
