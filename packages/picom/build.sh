7804f719462c8393617cd72b07094c4b1bd90361
Yisus7u7 <jesuspixel5@gmail.com>
2021-09-09 09:40:44 -0500

    fix build.sh

--- packages/picom/build.sh
+++ packages/picom/build.sh
@@ -3,8 +3,8 @@ TERMUX_PKG_DESCRIPTION="A lightweight compositor for X11"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@Yisus7u7 <jesuspixel5@gmail.com>"
 TERMUX_PKG_VERSION=8.2
-TERMUX_PKG_SRCURL=https://github.com/yshui/picom/archive/refs/tags/vNext.tar.gz
-TERMUX_PKG_SHA256=4ac9df246630c522bc4de5f56158ed4397bd12a6b954c5b7e688f1cbfe46cf0c
+TERMUX_PKG_SRCURL=https://github.com/yshui/picom/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=9d0c2533985e9670ff175e717a42b5bf1a2a00ccde5cac1e1009f5d6ee7912ec
 TERMUX_PKG_DEPENDS="libx11, libxcomposite, libxdamage, libxext, libxfixes, libxrender, xorgproto, xcb-proto, libconfig, libxdamage, libxcb, libxfixes, xcb-util-renderutil, libxrandr, libxcomposite, xcb-util-image, libxinerama, libpixman, dbus, pcre, libev"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS(){
