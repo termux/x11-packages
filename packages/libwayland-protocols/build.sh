7195433b23a1a303615796864f46a47ad9071bd2
Leap of Azzam <76893443+leapofazzam123@users.noreply.github.com>
2021-09-14 17:58:59 +0700

    Update build.sh

--- packages/libwayland-protocols/build.sh
+++ packages/libwayland-protocols/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://wayland.freedesktop.org/
 TERMUX_PKG_DESCRIPTION="Wayland protocols library"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.17
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_VERSION=1.21
 TERMUX_PKG_SRCURL=https://wayland.freedesktop.org/releases/wayland-protocols-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=df1319cf9705643aea9fd16f9056f4e5b2471bd10c0cc3713d4a4cdc23d6812f
+TERMUX_PKG_SHA256=b99945842d8be18817c26ee77dafa157883af89268e15f4a5a1a1ff3ffa4cde5
 TERMUX_PKG_DEPENDS="libandroid-support"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-documentation --with-host-scanner"
 
