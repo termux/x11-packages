848035ce77e788f42444c50ce810f251e3aaec21
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-09-20 20:49:15 +0300

    Update build.sh

--- packages/picom/build.sh
+++ packages/picom/build.sh
@@ -6,7 +6,4 @@ TERMUX_PKG_VERSION=8.2
 TERMUX_PKG_SRCURL=https://github.com/yshui/picom/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=9d0c2533985e9670ff175e717a42b5bf1a2a00ccde5cac1e1009f5d6ee7912ec
 TERMUX_PKG_DEPENDS="libx11, libxcomposite, libxdamage, libxext, libxfixes, libxrender, xorgproto, xcb-proto, libconfig, libxdamage, libxcb, libxfixes, xcb-util-renderutil, libxrandr, libxcomposite, xcb-util-image, libxinerama, libpixman, dbus, pcre, libev, uthash"
-
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS(){
-	-Dopengl=false
-	}
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dopengl=false"
