8f99a7d3cee99105b8c06411102276db0c0ef646
Yisus7u7 <64093255+Yisus7u7@users.noreply.github.com>
2021-09-20 11:56:40 -0500

    Change incorrect name
    This happened because of the autocorector on my keyboard

--- packages/picom/build.sh
+++ packages/picom/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_MAINTAINER="@Yisus7u7 <jesuspixel5@gmail.com>"
 TERMUX_PKG_VERSION=8.2
 TERMUX_PKG_SRCURL=https://github.com/yshui/picom/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=9d0c2533985e9670ff175e717a42b5bf1a2a00ccde5cac1e1009f5d6ee7912ec
-TERMUX_PKG_DEPENDS="libx11, libxcomposite, libxdamage, libxext, libxfixes, libxrender, xorgproto, xcb-proto, libconfig, libxdamage, libxcb, libxfixes, xcb-util-renderutil, libxrandr, libxcomposite, xcb-util-image, libxinerama, libpixman, dbus, pcre, libev, utash"
+TERMUX_PKG_DEPENDS="libx11, libxcomposite, libxdamage, libxext, libxfixes, libxrender, xorgproto, xcb-proto, libconfig, libxdamage, libxcb, libxfixes, xcb-util-renderutil, libxrandr, libxcomposite, xcb-util-image, libxinerama, libpixman, dbus, pcre, libev, uthash"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS(){
 	-Dopengl=false
