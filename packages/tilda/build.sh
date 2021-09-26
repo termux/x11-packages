3b56a9d3592f8413b196c49832745358b9cfe58f
ELWAER M <71197008+ELWAER-M@users.noreply.github.com>
2021-09-26 22:57:28 +0200

    fix...

--- packages/tilda/build.sh
+++ packages/tilda/build.sh
@@ -5,10 +5,10 @@ TERMUX_PKG_MAINTAINER="@ELWAER-M"
 TERMUX_PKG_VERSION="1.5.0"
 TERMUX_PKG_SRCURL="https://github.com/lanoxx/tilda/archive/refs/tags/tilda-${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256="f664c17daca2a2900f49de9eb65746ced03c867b02144149ef21260cbcd61039"
-TERMUX_PKG_DEPENDS="glib, gtk3, libvte, libconfuse, libx11, gettext, automake, autoconf"
-TERMUX_PKG_EXTRA_MAKE_ARGS="--silent"
+TERMUX_PKG_DEPENDS="glib, gtk3, libvte, libconfuse, libx11"
+TERMUX_PKG_BUILD_IN_SRC=true
 
 
 termux_step_pre_configure() {
-    NOCONFIFURE=1 ./autogen.sh
+    NOCONFIGURE=1 ./autogen.sh
 }
