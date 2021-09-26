514ffd2664186d5677746068b59adbd2eb370aee
WMCB-Tech <bautista.wyatt.marcus30@gmail.com>
2021-09-26 16:19:55 +0800

    scrot: add imlib2 as a dependency

--- packages/scrot/build.sh
+++ packages/scrot/build.sh
@@ -5,5 +5,5 @@ TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=0.10.0
 TERMUX_PKG_SRCURL="https://github.com/dreamer/scrot/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=c5f6b1adcd235c5e4368658593eef310385b95fc1a76b68ef6d633ad24800135
-TERMUX_PKG_DEPENDS="libxfixes, libx11"
+TERMUX_PKG_DEPENDS="imlib2, libxfixes, libx11"
 
