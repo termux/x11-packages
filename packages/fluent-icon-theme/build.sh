64315bf6b2e103c09c6cd9d15d6dbc42f8dfaf7c
Yisus7u7 <jesuspixel5@gmail.com>
2021-10-10 21:57:17 -0500

    add empty configure

--- packages/fluent-icon-theme/build.sh
+++ packages/fluent-icon-theme/build.sh
@@ -10,6 +10,10 @@ TERMUX_PKG_DEPENDS="hicolor-icon-theme"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
+termux_step_configure(){
+         :
+}
+
 termux_step_make_install(){
 	./install.sh -b -d=${TERMUX_PREFIX}/share/icons
 }
