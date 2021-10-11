af2b223ab436080b5a71ca05ab8029403af94e1e
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-10-11 20:17:11 +0300

    Update build.sh

--- packages/fluent-icon-theme/build.sh
+++ packages/fluent-icon-theme/build.sh
@@ -3,17 +3,13 @@ TERMUX_PKG_DESCRIPTION="Fluent icon theme for linux desktops"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@Yisus7u7 <jesuspixel5@gmail.com>"
 TERMUX_PKG_VERSION=2021.10.07
-_COMMIT=6399706c8c40686faac318f2a403fec23f70329c
-TERMUX_PKG_SRCURL=https://github.com/vinceliuice/Fluent-icon-theme/archive/${_COMMIT}.zip
-TERMUX_PKG_SHA256=f6cfd5047c1df143d9b24163c196b07b317d67caafb875d68b94c87239168513
+TERMUX_PKG_SRCURL=https://github.com/vinceliuice/Fluent-icon-theme/archive/${TERMUX_PKG_VERSION//./-}.tar.gz
+TERMUX_PKG_SHA256=d119e57f1612582dfd50ebc73565254d741af7e9489dbe39f21c3ec9b40b62b7
 TERMUX_PKG_DEPENDS="hicolor-icon-theme"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
-
-termux_step_configure(){
-         :
-}
+TERMUX_PKG_RM_ADTER_INSTALL="share/icons/*/icon-theme.cache"
 
 termux_step_make_install(){
-	./install.sh -b -d=${TERMUX_PREFIX}/share/icons
+	./install.sh -d ${TERMUX_PREFIX}/share/icons -b
 }
