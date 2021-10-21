7cd81dedcc8c2a15a50ad0179c7b3744c7e8ddf1
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-10-21 20:07:21 +0300

    Update build.sh

--- packages/bochs/build.sh
+++ packages/bochs/build.sh
@@ -25,6 +25,9 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-e1000
 --enable-clgd54xx
 --enable-voodoo
-CFLAGS+="-DANDROID"
-CXXFLAGS+="-DANDROID"
 "
+
+termux_step_pre_configure() {
+	CFLAGS+=" -DANDROID"
+	CXXFLAGS+=" -DANDROID"
+}
