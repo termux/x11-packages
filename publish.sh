987cd7dcebd9c6ec2b0afd9dec0b679a2023da97
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-10-26 21:35:21 +0000

    delete /publish.sh
    This script is no longer used.

--- publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-SCRIPT_NAME=$(basename "$0")
-REPOROOT=$(dirname "$(realpath "$0")")
-
-echo
-echo "Warning: this script is fine-tuned for repository setup by @xeffyr. It may not work for you."
-echo
-
-exec "$REPOROOT/scripts/bin/publish-repo"
