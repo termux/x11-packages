5ec67c085ced83cbaf2b0319305b9ece8de24cdc
Leap of Azzam <leapofazzam@gmail.com>
2021-10-31 10:41:26 +0700

    add package: lite-xl

---
+++ packages/lite-xl/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/lite-xl/lite-xl
+TERMUX_PKG_DESCRIPTION="A lightweight text editor written in Lua"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@leapofazzam123"
+TERMUX_PKG_VERSION=2.0.3
+TERMUX_PKG_SRCURL="https://github.com/lite-xl/lite-xl/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz"
+TERMUX_PKG_SHA256=6c8a4ea284f102f772e3aff583236e89d5b1171664526dd501000b681ae5c4e2
+TERMUX_PKG_DEPENDS="sdl2, freetype, liblua52"
+TERMUX_PKG_BUILD_IN_SRC=true
