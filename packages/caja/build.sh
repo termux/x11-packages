f178270645a14533152d5beb929bafd80fbd3d8b
Yisus7u7 <64093255+Yisus7u7@users.noreply.github.com>
2021-11-04 18:57:22 -0500

    Add startup-notification dependence

--- packages/caja/build.sh
+++ packages/caja/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_MAINTAINER="Yisus7u7 <jesuspixel5@gmail.com>"
 TERMUX_PKG_VERSION=1.24.0
 TERMUX_PKG_SRCURL=http://deb.debian.org/debian/pool/main/c/caja/caja_$TERMUX_PKG_VERSION.orig.tar.xz
 TERMUX_PKG_SHA256=7a176433f514127556bdbf31af694125a0a3dc7d4053888dd8da3e47c2d45652
-TERMUX_PKG_DEPENDS="dconf, gtk3, desktop-file-utils, atk, libcairo, librsvg, libpng, glib, mate-common, mate-desktop, libnotify, pango, libxml2, shared-mime-info, libexif"
+TERMUX_PKG_DEPENDS="dconf, gtk3, desktop-file-utils, atk, libcairo, librsvg, libpng, glib, mate-common, mate-desktop, libnotify, pango, libxml2, shared-mime-info, libexif, startup-notification"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-introspection=no"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_RM_AFTER_INSTALL="share/glib-2.0/schemas/gschemas.compiled"
