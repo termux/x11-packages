d6093f9abe22f289a91c7d68030381e005f2a56c
Leap of Azzam <leapofazzam@gmail.com>
2021-11-06 20:18:57 +0700

    hell

--- packages/lite-xl/build.sh
+++ packages/lite-xl/build.sh
@@ -6,3 +6,13 @@ TERMUX_PKG_VERSION=2.0.3
 TERMUX_PKG_SRCURL="https://github.com/lite-xl/lite-xl/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256=6c8a4ea284f102f772e3aff583236e89d5b1171664526dd501000b681ae5c4e2
 TERMUX_PKG_DEPENDS="ndk-sysroot, sdl2, freetype, liblua52"
+
+termux_step_pre_configure() {
+	# fix meson not detecting librt
+	cp $TERMUX_PREFIX/lib/librt.so $TERMUX_PREFIX/lib/librt.so.bak
+	echo 'INPUT(-lc)' > $TERMUX_PREFIX/lib/librt.so
+}
+
+termux_step_post_configure() {
+	mv $TERMUX_PREFIX/lib/librt.so.bak $TERMUX_PREFIX/lib/librt.so
+}
