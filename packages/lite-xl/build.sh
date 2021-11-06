3462b8f1e535a0837b437fb69ce748656d327e28
Leap of Azzam <leapofazzam@gmail.com>
2021-11-06 19:45:52 +0700

    nvm

--- packages/lite-xl/build.sh
+++ packages/lite-xl/build.sh
@@ -5,4 +5,4 @@ TERMUX_PKG_MAINTAINER="@leapofazzam123"
 TERMUX_PKG_VERSION=2.0.3
 TERMUX_PKG_SRCURL="https://github.com/lite-xl/lite-xl/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256=6c8a4ea284f102f772e3aff583236e89d5b1171664526dd501000b681ae5c4e2
-TERMUX_PKG_DEPENDS="sdl2, freetype, liblua52"
+TERMUX_PKG_DEPENDS="ndk-sysroot, sdl2, freetype, liblua52"
