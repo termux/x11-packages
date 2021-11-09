d547fec3c07a178206068d331afb557809b1ee01
T o x i c + K r o s t <89430597+AnGelXoG@users.noreply.github.com>
2021-11-09 15:31:25 -0600

    Update build.sh

--- packages/gnuboy/build.sh
+++ packages/gnuboy/build.sh
@@ -10,7 +10,7 @@ TERMUX_PKG_DEPENDS="sdl2"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
-        make -j $TERMUX_MAKE_PROCESSES -f Makefile.linux \
+        make -j $TERMUX_MAKE_PROCESSES -f Makefile.linux
 }
 
 termux_step_make_install() {
