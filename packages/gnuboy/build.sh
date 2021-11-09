f9b153322d78d15069acf52c37576fef5711d7da
T o x i c + K r o s t <89430597+AnGelXoG@users.noreply.github.com>
2021-11-09 17:17:52 -0600

    Update build.sh

--- packages/gnuboy/build.sh
+++ packages/gnuboy/build.sh
@@ -8,11 +8,3 @@ TERMUX_PKG_SRCURL=https://github.com/AlexOberhofer/SDL2-GNUBoy/archive/refs/tags
 TERMUX_PKG_SHA256=d8b729aa88747301ed39514ad9dc857b842332ac87242993881e15125af1be20
 TERMUX_PKG_DEPENDS="sdl2"
 TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_step_make() {
-        make -j $TERMUX_MAKE_PROCESSES -f Makefile.linux
-}
-
-termux_step_make_install() {
-        install -Dm700 -t $TERMUX_PREFIX/bin ./sdl2gnuboy
-}
