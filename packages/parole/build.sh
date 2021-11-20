c71431ef1cecb926eb1cb8acfff8a18f14ead91a
Yisus7u7 <64093255+Yisus7u7@users.noreply.github.com>
2021-11-20 14:16:40 -0500

    Fix dependencies
    XD

--- packages/parole/build.sh
+++ packages/parole/build.sh
@@ -6,5 +6,5 @@ TERMUX_PKG_VERSION=4.16.0
 TERMUX_PKG_SRCURL=http://deb.debian.org/debian/pool/main/p/parole/parole_${TERMUX_PKG_VERSION}.orig.tar.bz2
 TERMUX_PKG_SHA256=0d305ad8ccd3974d6b632f74325b1b8a39304c905c6b405b70f52c4cfd55a7e7
 # gstreamer all plugins for all support in parole
-TERMUX_PKG_DEPENDS="gtk3, gstreamer, gst-plugins-base, gst-plugins-bad, gst-plugins-god, gst-plugins-ugly, atk, libcairo, dbus-glib, libnotify, pango, libxfce4util, libxfce4ui, xfconf, taglib"
+TERMUX_PKG_DEPENDS="gtk3, gstreamer, gst-plugins-base, gst-plugins-bad, gst-plugins-good, gst-plugins-ugly, atk, libcairo, dbus-glib, libnotify, pango, libxfce4util, libxfce4ui, xfconf, taglib"
 TERMUX_PKG_BUILD_IN_SRC=true
