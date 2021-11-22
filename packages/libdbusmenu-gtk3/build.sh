8944848c5722584f511ebb4f69aed7bd2d3f8d14
Yisus7u7 <jesuspixel5@gmail.com>
2021-11-22 04:14:13 -0500

    add package: libdbusmenu-gtk3

---
+++ packages/libdbusmenu-gtk3/build.sh
@@ -0,0 +1,10 @@
+TERMUX_PKG_HOMEPAGE=https://launchpad.net/libdbusmenu
+TERMUX_PKG_DESCRIPTION="library for passing menus over DBus - GTK-3+ version"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="Yisus7u7 <jesuspixel5@gmail.com>"
+TERMUX_PKG_VERSION=18.10
+TERMUX_PKG_SRCURL=http://deb.debian.org/debian/pool/main/libd/libdbusmenu/libdbusmenu_${TERMUX_PKG_VERSION}.20180917~bzr492+repack1.orig.tar.xz
+TERMUX_PKG_SHA256=41298b926573419f21864205317461750b833c596af6ab0bd206e13336f8cee3
+TERMUX_PKG_DEPENDS="gtk3, glib, atk, libcairo, gdk-pixbuf, harfbuzz, pango"
+TERMUX_PKG_BUILD_DEPENDS="qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
+TERMUX_PKG_BUILD_IN_SRC=true
