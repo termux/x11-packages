b554dff958088a648c599a348a1c88ccca5d26d5
Yisus7u7 <jesuspixel5@gmail.com>
2021-11-22 04:30:44 -0500

    fix deps

--- packages/libdbusmenu-gtk3/build.sh
+++ packages/libdbusmenu-gtk3/build.sh
@@ -8,3 +8,6 @@ TERMUX_PKG_SHA256=41298b926573419f21864205317461750b833c596af6ab0bd206e13336f8ce
 TERMUX_PKG_DEPENDS="gtk3, glib, atk, libcairo, gdk-pixbuf, harfbuzz, pango, xsltproc, json-glib"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-introspection=no --disable-vala"
+termux_step_pre_configure(){
+	autoreconf -fi
+}
