81be6ac2d9ff138b4bfb63bb30d43047e4087fc5
Yisus7u7 <jesuspixel5@gmail.com>
2021-11-24 16:26:03 -0500

    add cmake and valac build depends

--- packages/cairo-dock-core/build.sh
+++ packages/cairo-dock-core/build.sh
@@ -7,6 +7,7 @@ _COMMIT=6c569e67a2a366e7634224a0133ede51755629cb
 TERMUX_PKG_SRCURL=https://github.com/Cairo-Dock/cairo-dock-core/archive/${_COMMIT}.zip
 TERMUX_PKG_SHA256=e59e99147ce9c901b46d4b56b88bd53aeda34292b86fd9fbf2c55d158153f2ec
 TERMUX_PKG_DEPENDS="atk, dbus-glib, glib, gtk3, pango, libcairo, libx11, xsltproc, libcurl, gdk-pixbuf, harfbuzz, mesa, glu, librsvg, libxcomposite, libwayland, libwayland-protocols, libxinerama, libxrandr, libxrender, libxtst, libxml2"
+TERMUX_PKG_BUILD_DEPENDS="valac, cmake"
 TERMUX_PKG_BUILD_IN_SRC=true
 termux_step_configure(){
 	mkdir build && cd build
