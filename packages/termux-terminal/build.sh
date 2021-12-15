465490b442888b19af28d03c6f276186277f9242
T o x i c <89430597+AnGelXoG@users.noreply.github.com>
2021-12-15 05:57:54 -0600

    Update build.sh

--- packages/termux-terminal/build.sh
+++ packages/termux-terminal/build.sh
@@ -7,3 +7,4 @@ TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/AnGelXoG/Termux-Terminal/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=f3518ea9fcd5767a8ef7a73bcb64014ba0bd2e57c072350d5356c918343f7f9c
 TERMUX_PKG_DEPENDS="libvte, make, cmake"
+TERMUX_PKG_BUILD_IN_SRC=true
