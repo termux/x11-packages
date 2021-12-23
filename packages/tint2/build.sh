cdaf070820e085ba2e2f6e59e7d57d02b7e94d98
WMCB-Tech <WMCB-Tech@users.noreply.github.com>
2021-12-23 17:53:31 +0800

    fix sha256

--- packages/tint2/build.sh
+++ packages/tint2/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=17.0.2
 TERMUX_PKG_SRCURL=https://gitlab.com/o9000/tint2/-/archive/${TERMUX_PKG_VERSION}/tint2-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=705a4505ab7a007e56d1c6dc2749ffb8eaabcc1d22f17ff2b2d29bdbeb0f28c5
+TERMUX_PKG_SHA256=f87f147e176d32f31f12fc1737f75f54c4c6f77961c6a2615ef9d64cb29ce24c
 TERMUX_PKG_DEPENDS="xorg-xrandr, libxinerama, libx11, xorgproto, libandroid-glob, libandroid-shmem, pango, libc++, libcairo, libcurl, libxcb, xcb-util-cursor, xcb-util-image, xcb-util-xrm, xcb-util-wm, pulseaudio, libxcomposite, libxdamage, gtk2, imlib2, libandroid-wordexp, librsvg, startup-notification, libcairo"
 TERMUX_PKG_BUILD_IN_SRC=true
 
