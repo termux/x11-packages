ef7818069f97326d1f24a58daaa62c2384be2121
WMCB-Tech <WMCB-Tech@users.noreply.github.com>
2021-12-23 17:44:51 +0800

    tint2: update package

--- packages/tint2/build.sh
+++ packages/tint2/build.sh
@@ -1,11 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://gitlab.com/o9000/tint2
 TERMUX_PKG_DESCRIPTION="Lightweight panel, Highly customizable"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_MAINTAINER="Laya Achmad"
-TERMUX_PKG_VERSION=16.2
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://github.com/o9000/tint2/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=a95c8a22d4cc117335b3157c7de48da91c8721fc97f86d0353c545eed2598ce8
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=17.0.2
+TERMUX_PKG_SRCURL=https://gitlab.com/o9000/tint2/-/archive/${TERMUX_PKG_VERSION}/tint2-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=705a4505ab7a007e56d1c6dc2749ffb8eaabcc1d22f17ff2b2d29bdbeb0f28c5
 TERMUX_PKG_DEPENDS="xorg-xrandr, libxinerama, libx11, xorgproto, libandroid-glob, libandroid-shmem, pango, libc++, libcairo, libcurl, libxcb, xcb-util-cursor, xcb-util-image, xcb-util-xrm, xcb-util-wm, pulseaudio, libxcomposite, libxdamage, gtk2, imlib2, libandroid-wordexp, librsvg, startup-notification, libcairo"
 TERMUX_PKG_BUILD_IN_SRC=true
 
