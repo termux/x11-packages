f96fad43a7c88674a857674027bb08cdad2ad396
Tee KOBAYASHI <xtkoba@gmail.com>
2022-02-28 04:10:24 +0900

    mpv-x: Do not link against libandroid

--- packages/mpv-x/build.sh
+++ packages/mpv-x/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Command-line media player"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.33.0
-TERMUX_PKG_REVISION=7
+TERMUX_PKG_REVISION=8
 TERMUX_PKG_SRCURL=https://github.com/mpv-player/mpv/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=f1b9baf5dc2eeaf376597c28a6281facf6ed98ff3d567e3955c95bf2459520b4
 TERMUX_PKG_DEPENDS="ffmpeg, libandroid-glob, libandroid-shmem, libarchive, libass, libdrm, littlecms, libjpeg-turbo, libcaca, liblua52, libx11, libxext, libxinerama, libxss, libxrandr, openal-soft, pulseaudio, zlib"
@@ -12,6 +12,13 @@ TERMUX_PKG_REPLACES="mpv"
 
 termux_step_pre_configure() {
 	LDFLAGS+=" -landroid-glob -landroid-shmem"
+
+	_NEED_DUMMY_LIBANDROID_A=
+	_LIBANDROID_A=$TERMUX_PREFIX/lib/libandroid.a
+	if [ ! -e $_LIBANDROID_A ]; then
+		_NEED_DUMMY_LIBANDROID_A=true
+		echo '!<arch>' > $_LIBANDROID_A
+	fi
 }
 
 termux_step_make_install() {
@@ -37,3 +44,10 @@ termux_step_make_install() {
 	install -Dm600 -t $TERMUX_PREFIX/etc/mpv/ $TERMUX_PKG_BUILDER_DIR/mpv.conf
 	install -Dm600 -t $TERMUX_PREFIX/share/mpv/scripts/ $TERMUX_PKG_SRCDIR/TOOLS/lua/*
 }
+
+termux_step_post_make_install() {
+	if [ $_NEED_DUMMY_LIBANDROID_A ]; then
+		rm -f $_LIBANDROID_A
+	fi
+}
+
