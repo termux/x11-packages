bfe6953452763524cf10ac2ea7858bc343243b19
Yonle <yonle@duck.com>
2022-03-01 10:45:22 +0700

    New package: SpaceCadetPinball
    Signed-off-by: Yonle <yonle@duck.com>

---
+++ packages/spacecadetpinball/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/k4zmu2a/SpaceCadetPinball
+TERMUX_PKG_DESCRIPTION="Decompilation of 3D Pinball for Windows – Space Cadet"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@Yonle <yonle@duck.com>"
+TERMUX_PKG_VERSION=2.0.1
+TERMUX_PKG_SRCURL=https://github.com/k4zmu2a/SpaceCadetPinball/archive/refs/tags/Release_${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=7b71815339c86a428d3569a5235c6ece0e2d4ff1d8025984ede6f772ca4f5423
+TERMUX_PKG_DEPENDS="sdl2, sdl2-mixer, xorgproto"
