31a43fc737fa42bc10a5da10d97e7a2d08c6329c
Yisus7u7 <dev.yisus@hotmail.com>
2022-04-17 13:33:18 -0500

    Update build.sh

--- packages/xfce4-docklike-plugin/build.sh
+++ packages/xfce4-docklike-plugin/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://docs.xfce.org/panel-plugins/xfce4-docklike-plugin/start
 TERMUX_PKG_DESCRIPTION="A modern, minimalist taskbar for Xfce."
 TERMUX_PKG_LICENSE="GPL-3.0"
-TERMUX_PKG_MAINTAINER="Yisus7u7 <dev.yisus@hotmail.com>"
+TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.4.0
 TERMUX_PKG_SRCURL=https://archive.xfce.org/src/panel-plugins/xfce4-docklike-plugin/0.4/xfce4-docklike-plugin-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=b4136a70897895f0599e8e7237223dde17221f099a2fc816917d5894bbd4f372
